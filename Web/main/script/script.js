36e09eeea1be641ae5cf66a06d36ad29718482d1
aimlinux <1k8aioon@gmail.com>
2026-02-27 16:24:13 +0900

    進んでいくよ

--- Web/main/script/script.js
+++ Web/main/script/script.js
@@ -1,3 +1,8 @@
-document.querySelector(".primary-btn").addEventListener("click", () => {
-  alert("保存機能は次で実装します 🎣");
+window.addEventListener("scroll", () => {
+  const nav = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    nav.style.background = "#1e5fa3";
+  } else {
+    nav.style.background = "linear-gradient(90deg, #2b7fc3, #5fa8e6)";
+  }
 });
