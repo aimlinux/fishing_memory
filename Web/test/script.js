7e9922744cac7b05a8d1e49cfcc4e18d463d9704
aimlinux <1k8aioon@gmail.com>
2026-03-19 17:15:39 +0900

    update pages.

--- Web/test/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", () => {
-  const nav = document.querySelector(".navbar");
-  if (window.scrollY > 50) {
-    nav.style.background = "#1e5fa3";
-  } else {
-    nav.style.background = "linear-gradient(90deg, #2b7fc3, #5fa8e6)";
-  }
-});
